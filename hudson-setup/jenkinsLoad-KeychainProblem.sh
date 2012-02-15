452b872581efbeba4b7468163eee68b2cc4dbbe6
Oleksandr Dodatko <dodikk88.reg@gmail.com>
2012-02-15 11:54:44 +0200

    apuz - research of keychain integration and hudson unix user setup

---
+++ hudson-setup/jenkinsLoad-KeychainProblem.sh
@@ -0,0 +1,3 @@
+epuadniw0070:LaunchDaemons chucknorris$ cd /Library/LaunchDaemons/
+epuadniw0070:LaunchDaemons chucknorris$ sudo launchctl unload org.jenkins-ci.plist 
+epuadniw0070:LaunchDaemons chucknorris$ sudo launchctl load org.jenkins-ci.plist
